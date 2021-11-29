97a0c973c69beac2dd850731512a1ee721f3e0b3
georgesmadi9 <geo.mad1699@gmail.com>
2021-11-29 20:34:36 +0200

    Minor fixes to the forms. Final Push.

--- src/main/java/com/camunda/ChoiceSaver.java
+++ src/main/java/com/camunda/ChoiceSaver.java
@@ -12,19 +12,157 @@ import java.util.List;
  // and save their choices in the GROUPS database
 public class ChoiceSaver implements JavaDelegate {
 
+     // creates the GROUPS Database if it does not exist
+    private void createDBGroups() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "create table if not exists GROUPS (CHOICEID int not null primary key auto_increment, PROJECTID int not null, STUDENTS varchar(255) not null);";
+        Statement st = conn.createStatement();
+        st.execute(query);
+
+        dbc.closeConnection(conn);
+    }
+
+    // checks if the database GROUPS is empty
+    private boolean isGroupsEmpty() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "select count(*) from GROUPS";
+        Statement st = conn.createStatement();
+        ResultSet res = st.executeQuery(query);
+
+        int count = 0;
+        while (res.next()) {
+            count = res.getInt("count");
+        }
+
+        dbc.closeConnection(conn);
+
+        return count == 0;
+    }
+
+     // default values initializer for the database (can be disabled)
+    private void initDbGroups() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "insert into GROUPS (projectid, students) values ( ?, ? )";
+        PreparedStatement pst = conn.prepareStatement(query);
+        pst.setInt(1,5);
+        pst.setString(2, "\"Jim Gaftet, Sarah Farah, Lechauna Mafwakandi\"");
+        pst.addBatch();
+        pst.setInt(1,8);
+        pst.setString(2, "\"Jim Gaftet, Sarah Farah, Lechauna Mafwakandi, Patricia Lagauffre\"");
+        pst.addBatch();
+        pst.setInt(1,10);
+        pst.setString(2, "\"Peter Harfouche, Yves Daaboul\"");
+        pst.addBatch();
+        pst.setInt(1,13);
+        pst.setString(2, "\"Peter Harfouche, Yves Daaboul, Mikhaela Charif, Theoura Tawahi\"");
+        pst.addBatch();
+        pst.setInt(1,8);
+        pst.setString(2, "\"Namadia Afwasahi, Laurent Mestret, Claude Dartoit, Koumba Baraji\"");
+        pst.addBatch();
+        pst.setInt(1,12);
+        pst.setString(2, "\"Namadia Afwasahi, Laurent Mestret, Claude Dartoit\"");
+        pst.addBatch();
+        pst.setInt(1,1);
+        pst.setString(2, "\"Vincent Delaprairie, Clemence Castel\"");
+        pst.addBatch();
+        pst.setInt(1,6);
+        pst.setString(2, "\"Vincent Delaprairie, Clemence Castel\"");
+        pst.addBatch();
+        pst.setInt(1,2);
+        pst.setString(2, "\"Alexandra Pronet, Clemntine Larage\"");
+        pst.addBatch();
+        pst.setInt(1,3);
+        pst.setString(2, "\"Alexandra Pronet, Clemntine Larage\"");
+        pst.addBatch();
+        pst.setInt(1,3);
+        pst.setString(2, "\"Raphael Sassine, Taylor Swift\"");
+        pst.addBatch();
+        pst.setInt(1,15);
+        pst.setString(2, "\"Raphael Sassine, Taylor Swift\"");
+        pst.addBatch();
+        pst.setInt(1,3);
+        pst.setString(2, "\"Raphael Sassine, Taylor Swift\"");
+        pst.addBatch();
+        pst.setInt(1,15);
+        pst.setString(2, "\"Raphael Sassine, Taylor Swift\"");
+        pst.addBatch();
+        pst.setInt(1,10);
+        pst.setString(2, "\"Magic Systeme, Eminem\"");
+        pst.addBatch();
+        pst.setInt(1,15);
+        pst.setString(2, "\"Magic Systeme, Eminem\"");
+        pst.addBatch();
+        pst.setInt(1,4);
+        pst.setString(2, "\"San Tahiti, Loic Frenel\"");
+        pst.addBatch();
+        pst.setInt(1,2);
+        pst.setString(2, "\"San Tahiti, Loic Frenel\"");
+        pst.addBatch();
+        pst.setInt(1,5);
+        pst.setString(2, "\"Mouafac Ghattas, Anas Daou, Karine Ismail\"");
+        pst.addBatch();
+        pst.setInt(1,8);
+        pst.setString(2, "\"Mouafac Ghattas, Anas Daou, Karine Ismail, Rola Hadi\"");
+        pst.addBatch();
+        pst.setInt(1,6);
+        pst.setString(2, "\"Maxime Ouisant, Maxine Bleze\"");
+        pst.addBatch();
+        pst.setInt(1,4);
+        pst.setString(2, "\"Maxime Ouisant, Maxine Bleze\"");
+        pst.addBatch();
+        pst.setInt(1,6);
+        pst.setString(2, "\"Robert De la Fistiniere, Sebastian Castellianos, Justine De Karakalla, Paola Sharabaty\"");
+        pst.addBatch();
+        pst.setInt(1,4);
+        pst.setString(2, "\"Robert De la Fistiniere, Sebastian Castellianos, Justine De Karakalla, Paola Sharabaty\"");
+        pst.addBatch();
+        pst.setInt(1,8);
+        pst.setString(2, "\"Carole De La Musique, Charlotte Ines, Chanis Mondi, Candice Boisson\"");
+        pst.addBatch();
+        pst.setInt(1,13);
+        pst.setString(2, "\"Carole De La Musique, Charlotte Ines, Chanis Mondi\"");
+        pst.addBatch();
+        pst.setInt(1,7);
+        pst.setString(2, "\"Christina Cordula, Stephane Plaza, Jasmine Baracuda\"");
+        pst.addBatch();
+        pst.setInt(1,9);
+        pst.setString(2, "\"Christina Cordula, Stephane Plaza, Jasmine Baracuda\"");
+        pst.addBatch();
+
+        pst.executeBatch();
+        conn.commit();
+        dbc.closeConnection(conn);
+
+    }
+
+    // gets lists of students in string format (name, name, ...) depending on the number of students for a project
     private String getOnly(List<String> names, int nbr) {
-        String s = "";
+        StringBuilder s = new StringBuilder();
         for (int i = 0; i < nbr; i++) {
             if (i == nbr - 1) {
-                s += names.get(i);
+                s.append(names.get(i));
             } else {
-                s += names.get(i) + ", ";
+                s.append(names.get(i)).append(", ");
             }
         }
-        return s;
+        return s.toString();
     }
 
     private String makeQuery(List<String> names, int grpc) throws SQLException {
+        // if database GROUPS does not exist it will be created
+        createDBGroups();
+
+        // to initialize the database values (disable if you want it to be empty)
+        if (isGroupsEmpty()) {
+            initDbGroups();
+        }
+
         DatabaseConnector dbc = new DatabaseConnector();
         Connection conn = dbc.createConnection();
 
@@ -74,7 +212,7 @@ public class ChoiceSaver implements JavaDelegate {
         String name3 = (String) delegateExecution.getVariable("name3");
         String name4 = (String) delegateExecution.getVariable("name4");
 
-        List<String> names = new ArrayList<String>();
+        List<String> names = new ArrayList<>();
         names.add(name1);
         names.add(name2);
         names.add(name3);
--- src/main/java/com/camunda/DatabaseWriter.java
+++ src/main/java/com/camunda/DatabaseWriter.java
@@ -3,14 +3,138 @@ package com.camunda;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 // Saves the inputted project into the database PROJECTS
 public class DatabaseWriter implements JavaDelegate {
 
+    // creates the PROJECTS Database if it does not exist
+    private void createDBProjects() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "create table if not exists PROJECTS (PROJECTID int not null primary key auto_increment, PROJECTNAME varchar(255) not null, STUDENTSNBR int not null, ADVISOR varchar(255) not null);";
+        Statement st = conn.createStatement();
+        st.execute(query);
+    }
+
+    // checks if the database PROJECTS is empty
+    private boolean isProjectsEmpty() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "select count(*) from PROJECTS";
+        Statement st = conn.createStatement();
+        ResultSet res = st.executeQuery(query);
+
+        int count = 0;
+        while (res.next()) {
+            count = res.getInt("count");
+        }
+
+        dbc.closeConnection(conn);
+
+        return count == 0;
+    }
+
+    // default values initializer for the database (can be disabled)
+    private void initDbProjects() throws SQLException {
+        DatabaseConnector dbc = new DatabaseConnector();
+        Connection conn = dbc.createConnection();
+
+        String query = "insert into PROJECTS (PROJECTNAME, STUDENTSNBR, ADVISOR) values ( ?, ?, ? )";
+        PreparedStatement pst = conn.prepareStatement(query);
+
+        pst.setString(1, "T'asCapté? An online transcriber using Deep Learning");
+        pst.setInt(2, 2);
+        pst.setString(3, "Youssef Bakouny");
+        pst.addBatch();
+
+        pst.setString(1, "SmartShip - The discovery of the Milky Way using CubeSat");
+        pst.setInt(2, 2);
+        pst.setString(3, "Elias Rachid");
+        pst.addBatch();
+
+        pst.setString(1, "TravelDream - An extended reality journey simulation");
+        pst.setInt(2, 2);
+        pst.setString(3, "Melhem Helou (El)");
+        pst.addBatch();
+
+        pst.setString(1, "AI Application for elderly people");
+        pst.setInt(2, 2);
+        pst.setString(3, "Rima Kilany Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "International Call - An application sharing worldwide issues");
+        pst.setInt(2, 3);
+        pst.setString(3, "Maroun Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "Meteo Prediction using Machine Learning");
+        pst.setInt(2, 2);
+        pst.setString(3, "Youssef Bakouny");
+        pst.addBatch();
+
+        pst.setString(1, "Station Spot - The placement of bike sharing systems");
+        pst.setInt(2, 3);
+        pst.setString(3, "Rima Kilany Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "Des ailes d'élite - IoT Network combining Middle Eastern airports");
+        pst.setInt(2, 4);
+        pst.setString(3, "Flavia Khatounian Rajji (El)");
+        pst.addBatch();
+
+        pst.setString(1, "Speed Tourism - Mobile App facilitating travel for people in a hurry");
+        pst.setInt(2, 3);
+        pst.setString(3, "Rima Kilany Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "An internal app for automation of HR rejection mails at Murex");
+        pst.setInt(2, 2);
+        pst.setString(3, "Youssef Bakouny");
+        pst.addBatch();
+
+        pst.setString(1, "Computer Vision and AI applied to Locksmith Industry");
+        pst.setInt(2, 3);
+        pst.setString(3, "Georges Sakr");
+        pst.addBatch();
+
+        pst.setString(1, "Shipping specific SAP ERP");
+        pst.setInt(2, 3);
+        pst.setString(3, "Marc Ibrahim");
+        pst.addBatch();
+
+        pst.setString(1, "Protecting C Applications using Blockchain Technology");
+        pst.setInt(2, 4);
+        pst.setString(3, "Maroun Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "How to deliver faster shipments internationally");
+        pst.setInt(2, 4);
+        pst.setString(3, "Maroun Chamoun");
+        pst.addBatch();
+
+        pst.setString(1, "White Music applied to Music Therapy");
+        pst.setInt(2, 2);
+        pst.setString(3, "Hadi Sawaya");
+        pst.addBatch();
+
+        pst.executeBatch();
+        conn.commit();
+        dbc.closeConnection(conn);
+
+    }
+
     private void writer(String projectname, int studentsnbr, String advisor) throws SQLException {
+        // if database GROUPS does not exist it will be created
+        createDBProjects();
+
+        // to initialize the database values (disable if you want it to be empty)
+        if (isProjectsEmpty()) {
+            initDbProjects();
+        }
+
         DatabaseConnector dbc = new DatabaseConnector();
         Connection conn = dbc.createConnection();
 
